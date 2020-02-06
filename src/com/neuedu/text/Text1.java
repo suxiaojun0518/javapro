91cdc7e5114f0bcce07e37adb880f002534ffe45
suxiaojun0518 <924869127@qq.con>
2020-02-06 15:34:57 +0800

    新文件

---
+++ src/com/neuedu/text/Text1.java
@@ -0,0 +1,10 @@
+package com.neuedu.text;
+
+public class Text1 {
+    public static void main(String[] args) {
+        int a=6;
+        for(int i=0;i<3;i++){
+            System.out.println(a);
+        }
+    }
+}
